fd1d838fb457e6a463b9fe540ff26c7ab0cb9f89
simonemagilla <simone.ferri+github@magilla.agency>
2022-12-22 15:58:26 +0100

    Creazione delle pagine e della tab del footer

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import Tab4 from './pages/Tab4';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -50,24 +51,35 @@ const App: React.FC = () => (
           <Route path="/tab3">
             <Tab3 />
           </Route>
+          <Route path="/tab4">
+            <Tab4 />
+          </Route>
+  
           <Route exact path="/">
             <Redirect to="/tab1" />
           </Route>
         </IonRouterOutlet>
+
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonLabel>Music</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonLabel>Shop</IonLabel>
           </IonTabButton>
+          <IonTabButton tab="tab4" href="/tab4">
+            <IonIcon icon={square} />
+            <IonLabel>Tour</IonLabel>
+          </IonTabButton>
+          
         </IonTabBar>
+
       </IonTabs>
     </IonReactRouter>
   </IonApp>
